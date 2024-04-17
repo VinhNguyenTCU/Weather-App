aa3562ae270209d7b3c34d99822228b10d047b4c
Vinh Nguyen <vinh29012005@gmail.com>
2024-04-16 20:43:57 -0500

    javascript function to change the city name and its information

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const apiKey = "b22e26db3bde7aa5dc4317cf09cef3c1";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchHa = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-    var data = await response.json();
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
+    const data = await response.json();
 
     console.log(data);
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = `${data.main.temp} &deg;C`;  
+    document.querySelector('.humidity').innerHTML = `${data.main.humidity}%`;
+    document.querySelector('.wind').innerHTML = `${Math.round(data.wind.speed)} km/h`;
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+        checkWeather(searchHa.value);
+});
